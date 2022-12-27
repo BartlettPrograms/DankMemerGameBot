8eb311209a69788c8368e3ec02bc3d6e2d40e5c7
BartlettPrograms <matt.bart33@gmail.com>
2022-12-27 21:33:02 +1300

    touch up

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 const { keyboard, Key, mouse, left, right, up, down, straightTo, centerOf, Region, sleep, Point} = require("@nut-tree/nut-js")
 
 const farmingValues = ["fish", "hunt", "dig", "beg", "highlow"]
+// Wait times
 const waitDiscordTextDetect = 3000
 const waitNetworkSpeedTimer = 3000
 const waitHighLowTimer = 3000
 const waitDankMemerCooldown = 36000
-//keyboard.config.autoDelayMs = 
-
+// Positions
 const posMessageBar = new Point(520, 1000)
 const posHighLowJackpotButton = new Point(520, 930)
+
 
 
 // Typing function
@@ -25,6 +26,11 @@ const typing = async (str, wait) => {
 }
 
 
+
+mainLoop();
+
+
+
 // Main Loop
 const mainLoop = async () =>
 {
@@ -34,7 +40,7 @@ const mainLoop = async () =>
     await typing(farmingValues[3], waitDiscordTextDetect)
     setTimeout(callHighlow, waitNetworkSpeedTimer)
 }
-
+//Calls highlow
 const callHighlow = async () =>
 {
     await keyboard.type(Key.Enter)
@@ -42,7 +48,7 @@ const callHighlow = async () =>
     await typing(farmingValues[4], waitDiscordTextDetect)
     setTimeout(doHighlow, waitHighLowTimer)
 }
-
+//Completes highlow task
 const doHighlow = async () => 
 {
     await mouse.move(
@@ -53,7 +59,7 @@ const doHighlow = async () =>
     setTimeout(mainLoop, waitDankMemerCooldown)
 }
 
-mainLoop();
+
 
 
 
